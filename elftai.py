aeb4f07e769f0c460dbef8cf08e03d4e21537c8a
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-12 17:53:48 -0300

    Improvements to argument parsing

--- elftai.py
+++ elftai.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import sys
 from argparse import ArgumentParser
 
 from TitleManager import TitleManager
@@ -10,20 +11,25 @@ def main():
     
     list_parser = subp.add_parser('list', aliases=['l'], help='List information about one or all Titles')
     list_parser.add_argument('-e', '--entries', nargs='?', default=5, type=int, help='Number of latest items to print')
-    list_parser.add_argument('names', nargs='*', help='Titles about which to print specific information. If none, print summary about all Titles.')
+    list_parser.add_argument('name', nargs='*', help='Titles about which to print specific information. If none is given, print summary about all Titles.')
     list_parser.set_defaults(func=parse_list)
 
     args = parser.parse_args()
-    args.func(args)
-
-def parse_list(args):
     with TitleManager('test.csv', 'test_out.csv') as tm:
-        if len(args.names) != 0:
-            for name in args.names:
-                if name != args.names[0]: print()
-                # tm.printsummary
-        else:
-            tm.print_summary()
+        try:
+            args.func(args, tm)
+        except:
+            print("Wrong command line operation. Try running '{} -h'".format(sys.argv[0]))
+
+def parse_list(args, tm):
+    if len(args.name) != 0:
+        for name in args.name:
+            if name != args.name[0]: print()
+            ok = tm.print_full(name.strip())
+            if not ok:
+                print("Could not find Title by name/alias '{}'".format(name.strip()))
+    else:
+        tm.print_summary()
 
 if __name__=='__main__':
     main()
