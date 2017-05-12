9574d457f1e927ce75fc107e31a258a8ebfe9acf
Matheus Saldanha <matheus.saldanha@usp.br>
2017-05-12 18:06:03 -0300

    Add parsing for operation find

--- elftai.py
+++ elftai.py
@@ -6,20 +6,25 @@ from argparse import ArgumentParser
 from TitleManager import TitleManager
 
 def main():
-    parser = ArgumentParser(allow_abbrev=True)
+    parser = ArgumentParser(description="ElfTAI (Title/Alias/Items) - Program for organizing data with specific characteristics.", allow_abbrev=True)
     subp = parser.add_subparsers()
     
-    list_parser = subp.add_parser('list', aliases=['l'], help='List information about one or all Titles')
+    list_parser = subp.add_parser('list', help='List information about one or all Titles')
     list_parser.add_argument('-e', '--entries', nargs='?', default=5, type=int, help='Number of latest items to print')
-    list_parser.add_argument('name', nargs='*', help='Titles about which to print specific information. If none is given, print summary about all Titles.')
+    list_parser.add_argument('name', nargs="*", help='Titles about which to print specific information. If none is given, print summary about all Titles.')
     list_parser.set_defaults(func=parse_list)
+
+    find_parser = subp.add_parser('find', help='Display Titles that contain the given item.')
+    find_parser.add_argument('item', nargs=1, help='IDs to find')
+    find_parser.set_defaults(func=parse_find)
 
     args = parser.parse_args()
     with TitleManager('test.csv', 'test_out.csv') as tm:
-        try:
-            args.func(args, tm)
-        except:
-            print("Wrong command line operation. Try running '{} -h'".format(sys.argv[0]))
+        args.func(args, tm)
+        #try:
+        #    args.func(args, tm)
+        #except:
+        #    print("Wrong command line operation. Try running '{} -h'".format(sys.argv[0]))
 
 def parse_list(args, tm):
     if len(args.name) != 0:
@@ -30,6 +35,10 @@ def parse_list(args, tm):
                 print("Could not find Title by name/alias '{}'".format(name.strip()))
     else:
         tm.print_summary()
+
+def parse_find(args, tm):
+    tm.find_item(args.item[0])
+
 
 if __name__=='__main__':
     main()
